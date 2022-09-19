2b2367e8f48624da228f79ccc8d2cacccefd91af
Yeiser1 <archundiadaniel17@gmail.com>
2022-09-18 22:05:25 -0500

    agregando cokies

---
+++ Cokies/src/main/java/mx/uaemex/fi/ingenieria_soft_II/gamesa/Cocinero.java
@@ -0,0 +1,47 @@
+package mx.uaemex.fi.ingenieria_soft_II.gamesa;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class Cocinero
+ */
+public class Cocinero extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw;
+		Cookie galleta;
+		String sabor;
+		
+		//procesar el formulario
+		sabor = request.getParameter("sabor");
+		
+		galleta = new Cookie("sabor",sabor);
+		response.addCookie(galleta);
+		
+		galleta.setValue(sabor);
+		response.setContentType("text/html");
+		pw = response.getWriter();
+		pw.println("<!DOCTYPE html>");
+		pw.println("<html>");
+		pw.println("<head>");
+		pw.println("<title>Cocina</title>");
+		pw.println("</head>");
+		pw.println("<body>");
+		pw.println("<p>Gracias por compartir sus preferencias</p>");
+		pw.println("<a href=\"index.html\">Regresar</a>");
+		pw.println("</body>");
+		pw.println("</html>");
+		
+	}
+}
---
+++ Cokies/src/main/java/mx/uaemex/fi/ingenieria_soft_II/gamesa/Mesero.java
@@ -0,0 +1,57 @@
+package mx.uaemex.fi.ingenieria_soft_II.gamesa;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class Mesero
+ */
+public class Mesero extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter pw;
+		Cookie[] tarro;
+		String sabor="";
+		
+		//procesar las COOKIES
+		tarro = request.getCookies();
+		if(tarro!=null) {
+		for (Cookie g:tarro) {
+			if (g.getName().compareTo("sabor")==0) {
+				sabor= g.getValue();
+				
+			}
+		}
+		}
+		response.setContentType("text/html");
+		pw = response.getWriter();
+		pw.println("<!DOCTYPE html>");
+		pw.println("<html>");
+		pw.println("<head>");
+		pw.println("<title>Cocina</title>");
+		pw.println("</head>");
+		pw.println("<body>");
+		if(sabor.length()>0) {
+			pw.println("<p>Tenemos 14 galletas, sabor: ->"+sabor+"<- especialmente para ti</p>");
+			pw.println("<a href=\"index.html\">Regresar</a>");
+		}else {
+			pw.println("<p>Para prepararte las galletas debes seleccionar el sabor");
+			pw.println("<a href=\"index.html\">Ir a seleccionar</a>");
+		}
+		pw.println("</body>");
+		pw.println("</html>");
+		
+	}
+
+}
